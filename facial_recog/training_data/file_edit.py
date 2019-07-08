555880c7520c07af9e2779e920d49107aedee1f6
pranmar123 <pranmar123@gmail.com>
2019-07-08 11:00:08 -0500

    Add file_edit.py

---
+++ facial_recog/training_data/file_edit.py
@@ -0,0 +1,30 @@
+import os
+from time import sleep
+
+def main():
+
+    for filename in os.listdir("/home/pranmar123/Downloads/dataset"):
+        newdir = "/home/pranmar123/Downloads/dataset/"+filename
+        #going into the folders
+        if os.path.isdir(newdir) == True:
+            os.chdir(newdir)
+            test = os.listdir(newdir)           
+            #removing .txt
+            for i in test:
+                if i.endswith(".txt"):
+                    os.remove(os.path.join(newdir, i))
+            #removing every files
+            test = os.listdir(newdir)
+            for i in test[::2]:
+                os.remove(i)
+                print("Removed file: ",i)
+            #renaming the leftover files
+            test = os.listdir(newdir)
+            count = 0
+            for i in test:
+                dst = str(count) + ".jpg" 
+                os.rename(i,dst)
+                count += 1
+                
+
+main()
