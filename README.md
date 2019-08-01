7e5f7be0f479bd6c82d69a2d0501903367db8cd2
pranmar123 <pranmar123@gmail.com>
2019-08-01 16:08:36 -0500

    Update readme.md and create new LSB

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Multi-Facial-Steganography
-REU Project
+
+<Title of REU Project>
+=================
+
+
+Information
+-----------
+
+LSBSteg module is based on OpenCV to hide data in images. It uses the first bit of every pixel, and every colour
+of an image. The code is quite simple to understand; If every first bit has been used, the module starts using the second bit, so the larger the data, the more the image is altered.
+The program can hide all of the data if there is enough space in the image. The main functions are:
+
+* encode_text: You provide a string and the program hides it
+
+
+JPEG and otehr lossy compression formats are not supported. 
+
+Installation
+------------
+
+This tool only require OpenCV and its dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+
+Usage
+-----
--- facial_recog/steg.py
+++ facial_recog/steg.py
@@ -17,7 +17,6 @@ def main():
     else:
         menuDecode()
 
-
 def menuEncode():
     picture, imgPath = facial_features.select_image()
     chosenFeature, pointsList, pixelsList = facial_features.do_facial_feature_recog(picture, imgPath)
@@ -31,8 +30,8 @@ def menuDecode():
     #imgPath = str(input("Enter the path to the image: "))
     #facialFeature = str(input("Enter the facial feature (eyes, mouth, nose): "))
     #we are passing 1 in to the facial_feature_recog function to tell that function to decode
-    picture = 'newimage.png'
-    imgPath = '/home/pranmar123/Multi-Facial-Steganography/facial_recog/dataset/newimage.png'
+    picture = '1.png'
+    imgPath = '/home/pranmar123/Multi-Facial-Steganography/facial_recog/dataset/1.png'
     facialFeature = 'mouth'
     pointsList = facial_features.do_facial_feature_recog(picture, imgPath, 1, facialFeature)
     pointsList = pointsList[1]
